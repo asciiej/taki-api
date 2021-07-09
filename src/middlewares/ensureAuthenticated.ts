c103873cc7a7046a3c1a4dc3549d20c474939b9f
Kaio Souza <kaioaugusto02@gmail.com>
2021-07-08 23:05:35 -0300

    🔒 Verificar se o usuário logado

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+  try {
+    const decoded = verify(token, process.env.PRIVATE_KEY);
+
+    const { sub } = decoded as TokenPayload;
+    request.user = {
+      id: sub,
+    };
+  } catch (e) {
+    throw new Error('Invalid token');
+  }
+}
