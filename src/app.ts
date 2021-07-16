e3d6c7e5aa53296d98add2b2cd82f4fe5c3a7c4d
Kaio Souza <kaioaugusto02@gmail.com>
2021-07-16 00:48:59 -0300

    ➕ Adicionando dependencia cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import router from './routes/routes';
 
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
